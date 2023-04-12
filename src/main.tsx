31db37a4c3078df9eb1de547e4fa33c9c772547e
Lucas Barcelos <lucasvm87@gmail.com>
2023-04-12 11:31:43 -0300

    inserting others things cute

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {Tweet} from './components/Tweet'
 import './main.css'
+import twitterLogo from './assets/logo-twitter.svg'
 //components: pequenas partes de interface reutilizaveis
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <div className="layout">
-      <aside className="sidebar">sidebar
+      <aside className="sidebar">
+      <img className="logo" src={twitterLogo} alt="logo" />
+      
+      <nav className="main-navigation">
+      <a href="">Home</a>
+      <a href="">Explore</a>
+      <a href="">Notifications</a>
+      <a href="">Messages</a>
+      <a href="">Bookmarks</a> 
+      <a href="">Lists</a>
+      <a href="">Profile</a>
+      <a href="">More</a>
+      </nav>
+
+      <button className="new-tweet" type="button">Tweet</button>
       </aside>
-      <div className="content">content
+      <div className="content">
 
       </div>
     </div>
