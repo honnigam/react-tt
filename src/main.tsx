0bafe7a7c813b7bccd622d9771900cc5cf7a49b2
Lucas Barcelos <lucasvm87@gmail.com>
2023-04-12 09:04:35 -0300

    insert a css styles

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,11 @@ import './main.css'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <div className="layout">
-      <aside className="sidebar"></aside>
-      <div className="content"></div>
+      <aside className="sidebar">sidebar
+      </aside>
+      <div className="content">content
+
+      </div>
     </div>
   </React.StrictMode>,
 )
