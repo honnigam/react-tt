78673660c54f6babb87ae31df78c4a7708c945f4
Lucas Barcelos <lucasvm87@gmail.com>
2023-04-14 14:39:38 -0300

    componentes Header and Sidebar

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import { Sparkle } from "phosphor-react";
+import './Header.css'
+
+export function Header() {
+  return (          
+    <div className="timeline-header">
+    Home
+    <Sparkle />
+</div>)
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,58 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {Tweet} from './components/Tweet'
 import './main.css'
-import {House, Hash, Envelope, Bell, BookmarkSimple, FileText, User, DotsThreeCircle, Sparkle} from 'phosphor-react'
 import twitterLogo from './assets/logo-twitter.svg'
+import { Sidebar } from './components/Sidebar'
+import { Header } from './components/Header'
 //components: pequenas partes de interface reutilizaveis
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <div className="layout">
-      <aside className="sidebar">
-      <img className="logo" src={twitterLogo} alt="logo" />
-      
-      <nav className="main-navigation">
-      <a className="active" href="">
-        <House weight="fill"/> Home
-      </a>
-      <a href="">
-        <Hash /> Explore
-      </a>
-      <a href="">
-        <Bell/>
-        Notifications
-      </a>
-      <a href="">
-        <Envelope />
-        Messages
-      </a>
-      <a href="">
-        <BookmarkSimple />
-        Bookmarks
-      </a> 
-      <a href="">
-        <FileText />
-        Lists
-      </a>
-      <a href="">
-        <User />
-        Profile
-      </a>
-      <a href="">
-        <DotsThreeCircle />
-        More
-      </a>
-      </nav>
-
-      <button className="new-tweet" type="button">Tweet</button>
-      </aside>
+      <Sidebar />
       <div className="content">
         <main className="timeline">
-          <div className="timeline-header">
-            Home
-          <Sparkle />
-          </div>
-
+          <Header/>
+          
           <form className="new-tweet-form">
             <label htmlFor="tweet">
               <img src="https://github.com/honnigam.png" alt="Lucas Barcelos" />
